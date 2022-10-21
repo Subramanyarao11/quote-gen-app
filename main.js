4f99db3b40ac94d7580403fe1e88d58dc042a9f8
Subramanyarao11 <subramanya11rao@gmail.com>
2022-10-22 00:51:13 +0530

    API

---
+++ main.js
@@ -0,0 +1,19 @@
+const api = "https://api.quotable.io/random";
+
+// Grabbing Quote and Author elements
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+const btn = document.getElementById("btn");
+
+// Attaching Event Listener to Button
+
+btn.addEventListener("click", getQuote);
+
+function getQuote() {
+    fetch(api)
+      .then((res) => res.json())
+      .then((data) => {
+        quote.innerHTML = `"${data.content}"`;
+        author.innerHTML = `- ${data.author}`;
+      });
+  }
